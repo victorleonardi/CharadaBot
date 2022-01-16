5d240ed3c58fcabc4649d36e3ba9eaf506eb79b0
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2022-01-16 17:26:24 -0300

    Atualizações no arquivo charada.py

--- cogs/charada.py
+++ cogs/charada.py
@@ -1,3 +1,48 @@
 import discord
 import asyncio
+from random import randint
 from discord.ext import commands
+
+
+class Charada(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    # Setup
+    def setup(self):
+        self.list_of_all_riddles = [
+            ('eco', 'Eu falo sem boca e ouço sem ouvidos. Não tenho corpo, mas com o ar me torno vivo.'),
+            ('vela', 'Você mede minha vida em horas e eu o sirvo expirando. Sou rápido quando fino e lento quando grosso. O vento é meu inimigo'),
+            ('mapa', 'Eu tenho cidades, mas não casa. Eu tenho montanhas, mas não árvores. Eu tenho água, mas nenhum peixe.'),
+            ('r', 'O que é visto no meio de Março e Abril e não pode ser visto no inicio ou fim de ambos os meses?'),
+            ('selo', 'Sem sair do meu espaço, sou capaz de viajar por todo o mundo.'),
+            ('gelo', 'Dela sou feito, porém, se nela me encontrar, em questão de tempo eu desapareço.'),
+            ('papel', 'Aguento qualquer queda sem problemas. Contudo, no mais subito respingo, me ruino.'),
+            ('respiração', 'Sou sutil e leve como uma pena. Contudo, nem mesmo o mais musculoso é capaz de me segurar por mais de um minuto.')
+        ]
+        self.list_of_riddles_used_in_the_month = []
+
+    # Events
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Bot is online')
+
+    # Commands
+    @commands.command()
+    async def tell_riddle(self, ctx):
+        n = randint(0, (len(self.list_of_all_riddles) - 1))
+        self.answer_riddle, text_riddle = self.list_of_all_riddles[n]
+        self.list_of_riddles_used_in_the_month.append(
+            self.list_of_all_riddles[n])
+        self.list_of_all_riddles.remove(self.list_of_all_riddles[n])
+        await ctx.send(text_riddle)
+
+    @commands.command()
+    async def answer(self, ctx, resposta: str, cargo: str, user: discord.Member):
+        resposta_lit = str(resposta).lower
+        if resposta_lit in self.answer_riddle:
+            given_role = discord.utils.get(ctx.guild.roles, name=cargo)
+            await user.add_roles(given_role)
+        if resposta_lit not in self.answer_riddle:
+            await ctx.send('Infelizmente, você errou...')
---
+++ test.py
@@ -0,0 +1,7 @@
+from random import randint
+
+dic_test = {'ola': 'tchau', 'Tchau': "Olá"}
+
+n = randint(0, 1)
+
+print(dic_test[n])
